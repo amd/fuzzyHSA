5f57cd55c1fdbe1e6cb2c6c5a17abf26ff5d9e03
Zachary Streeter <zstreete@amd.com>
2024-04-25 13:29:31 -0500

    added hsa generation to autogen_stubs and refactored

--- autogen_stubs.sh
+++ autogen_stubs.sh
@@ -31,57 +31,119 @@ fixup() {
 }
 
 function check_generated_file_existence() {
-    local filename=$1
-    local full_path="$BASE/$filename"
-    if [[ -f "$full_path" ]]; then
-        return 0
-    else
-        return 1
-    fi
+	local filename=$1
+	local full_path="$BASE/$filename"
+	if [[ -f "$full_path" ]]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+function generate_amd_gpu() {
+	local filename="amd_gpu.py"
+	if check_generated_file_existence "$filename"; then
+		echo "$filename already exists. Skipping generation."
+	else
+		local rocm_path="/opt/rocm"
+		local include_path="$rocm_path/include"
+
+		wget https://raw.githubusercontent.com/ROCm/ROCR-Runtime/201228c4fbd343cebdb6457ded7cb4d55637d60d/src/core/inc/sdma_registers.h -O $BASE/sdma_registers.h
+		clang2py $BASE/sdma_registers.h --clang-args="-I$include_path -x c++" -o "$BASE/amd_gpu.py" -l /opt/rocm/lib/libhsa-runtime64.so
+
+    NVD_HEADER=$(find /usr/src -name nvd.h | grep 'amdgpu')
+    [ -f "NVD_HEADER" ] && echo "Couldn't find nvd.h on the system" && exit 1
+
+		sed 's/^\(.*\)\(\s*\/\*\)\(.*\)$/\1 #\2\3/; s/^\(\s*\*\)\(.*\)$/#\1\2/' $NVD_HEADER >>$BASE/amd_gpu.py # comments
+		sed -i 's/#\s*define\s*\([^ \t]*\)(\([^)]*\))\s*\(.*\)/def \1(\2): return \3/' $BASE/amd_gpu.py        # #define name(x) (smth) -> def name(x): return (smth)
+		sed -i '/#\s*define\s\+\([^ \t]\+\)\s\+\([^ ]\+\)/s//\1 = \2/' $BASE/amd_gpu.py                        # #define name val -> name = val
+
+		fixup "$BASE/amd_gpu.py"
+
+		echo "Installed $filename at $BASE"
+	fi
+}
+
+function generate_hsa() {
+	local filename="hsa.py"
+	if check_generated_file_existence "$filename"; then
+		echo "$filename already exists. Skipping generation."
+	else
+		local include_path="/opt/rocm/include"
+		local lib_path="/opt/rocm/lib"
+
+		clang2py \
+			$include_path/hsa/hsa.h \
+			$include_path/hsa/hsa_ext_amd.h \
+			$include_path/hsa/amd_hsa_signal.h \
+			$include_path/hsa/amd_hsa_queue.h \
+			$include_path/hsa/amd_hsa_kernel_code.h \
+			$include_path/hsa/hsa_ext_finalize.h \
+			$include_path/hsa/hsa_ext_image.h \
+			$include_path/hsa/hsa_ven_amd_aqlprofile.h \
+			--clang-args=-I"$include_path" -o "$BASE/$filename" -l "$lib_path"/libhsa-runtime64.so
+
+		sed -i "s\import ctypes\import ctypes, os\g" $BASE/$filename
+		sed -i "s\'/opt/rocm/\os.getenv('ROCM_PATH', '/opt/rocm/')+'/\g" $BASE/$filename
+
+		fixup "$BASE/$filename"
+
+		echo "Installed $filename at $BASE"
+	fi
+}
+
+function generate_kfd() {
+	local filename="kfd.py"
+	if check_generated_file_existence "$filename"; then
+		echo "$filename already exists. Skipping generation."
+	else
+		clang2py \
+			"/usr/include/linux/kfd_ioctl.h" \
+			-o "$BASE"/"$filename" \
+			-k cdefstum
+
+		sed -i "s\import ctypes\import ctypes, os\g" "$BASE"/"$filename"
+
+		fixup "$BASE"/"$filename"
+
+		echo "Installed $filename at $BASE"
+	fi
 }
 
 function generate() {
-    local filename="$1.py"
-    if check_generated_file_existence "$filename"; then
-        echo "$filename already exists. Skipping generation."
-    else
-      clang2py \
-        "/usr/include/linux/kfd_ioctl.h" \
-        -o "$BASE"/"$filename" \
-        -k cdefstum #TODO: need to have this more general so it uses in "$filename"
-      fixup "$BASE"/"$filename"
-      sed -i "s\import ctypes\import ctypes, os\g" "$BASE"/"$filename"
-      echo "Installed $filename at '$BASE'"
-    fi
+	generate_amd_gpu
+	generate_hsa
+	generate_kfd
+}
+
+function delete_generated_file() {
+	local filename="$1"
+	if check_generated_file_existence "$filename"; then
+		echo "Cleaning up: removing $filename"
+		rm "$BASE"/"$filename"
+	else
+		echo "No cleanup needed. $filename does not exist."
+	fi
 }
 
 function clean() {
-    local filename="$1.py"
-    if check_generated_file_existence "$filename"; then
-          echo "Cleaning up: removing $filename"
-          rm "$BASE"/"$filename"
-      else
-          echo "No cleanup needed. $filename does not exist."
-      fi
+	delete_generated_file "kfd.py"
+	delete_generated_file "hsa.py"
+	delete_generated_file "amd_gpu.py"
+	delete_generated_file "sdma_registers.h"
 }
 
-function parse_arg() {
-    IFS='=' read -ra ADDR <<< "$1"  # Split argument by '='
-    local command="${ADDR[0]}"
-    local value="${ADDR[1]}"
-
-    case $command in
-        generate)
-            generate "$value"
-            ;;
-        clean)
-            clean "$value"
-            ;;
-        *)
-            echo "Unknown command: $command"
-            exit 1
-            ;;
-    esac
-}
-
-parse_arg "$1"
+case "$1" in
+generate)
+	generate
+	;;
+clean)
+	clean
+	;;
+*)
+	echo "Usage: $0 [generate|clean]"
+	echo "generate: Prepares and creates bindings for kfd and HSA, converting system headers into usable Python modules."
+	echo "clean: Removes any previously generated files to ensure a clean state for re-generation."
+	exit 1
+	;;
+esac
